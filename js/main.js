8c713b837d74abe26f445d9d90e61c96fc85efb7
gabrielRamos505 <rustandree529@gmail.com>
2026-09-05 20:46:45 -0500

    paso 1

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const canvas = document.getElementById("glCanvas");
+
+const gl = canvas.getContext("webgl2");
+
+if (!gl) {
+    throw new Error("WebGL2 no está disponible en este navegador.");
+}
+
+gl.viewport(0, 0, canvas.width, canvas.height);
+
+gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+gl.clear(gl.COLOR_BUFFER_BIT);
